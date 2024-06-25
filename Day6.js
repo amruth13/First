96934e15f1f486cba77809c441500f967f4fcfd1
AmruthAmulya <amruth13>
2024-06-25 08:42:55 -0400

    "some new functions"

--- Day6.js
+++ Day6.js
@@ -1,15 +1,18 @@
-let objarr=[]
-obj=$('<div>')
 
 
-// objarr.push(new CreateInput({}))
-objarr.push(new CreateInput({ "inputType": "date" }))
 
-objarr.push(new CreateInput({ "inputType": "datetime" }))
+// let objarr=[]
+// obj=$('<div>')
 
-objarr.push(new CreateInput({ "inputType": "time" }))
-objarr.forEach(function (x,i){
-console.log(x);
-$("body").append(x.getDesigen())
-})
-console.log(objarr);
+
+
+// objarr.push(new CreateInput({ "inputType": "date" }))
+
+// objarr.push(new CreateInput({ "inputType": "datetime" }))
+
+// objarr.push(new CreateInput({ "inputType": "time" }))
+// objarr.forEach(function (x,i){
+// console.log(x);
+// $("body").append(x.getDesigen())
+// })
+// console.log(objarr);
