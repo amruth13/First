9dbce2eacb05a0595bdbec93329042687cf07ae3
AmruthAmulya <amruth13>
2024-06-26 03:12:46 -0400

    "completed"

--- Day6.js
+++ Day6.js
@@ -6,11 +6,11 @@
 
 
 
-// objarr.push(new CreateInput({ "inputType": "date" }))
+// objarr.push(new clsCreateInput({ "inputType": "date" }))
 
-// objarr.push(new CreateInput({ "inputType": "datetime" }))
+// objarr.push(new clsCreateInput({ "inputType": "datetime" }))
 
-// objarr.push(new CreateInput({ "inputType": "time" }))
+// objarr.push(new clsCreateInput({ "inputType": "time" }))
 // objarr.forEach(function (x,i){
 // console.log(x);
 // $("body").append(x.getDesigen())
