4c151a556d9efa0d538c9aacb7c674cb764709d5
AmruthAmulya <amruth13>
2024-06-27 02:27:17 -0400

    "thursday 27/06/2024"

--- Day6.js
+++ Day6.js
@@ -1,18 +1,46 @@
+let objarr = [];
+obj = $("<div>");
 
+objarr.push(
+  new clsCreateInput({
+    inputType: "time",
+    labelTime: "time",
+    descTime: "time is imp",
+    Suppresslabel: true,
+    toolTipText: "time",
+    Position: "right",
+    newLabel: "hello",
+  })
+);
 
+objarr.push(
+  new clsCreateInput({
+    inputType: "datetime",
+    labelDateTime: "datetime",
+    descTimeDate: "date and time",
+    Suppresslabel: true,
+    toolTipText: "Date and time",
+    Position: "top",
+    newLabel: "hello",
+  })
+);
 
-// let objarr=[]
-// obj=$('<div>')
+objarr.push(
+  new clsCreateInput({
+    inputType: "date",
+    labelDate: "date",
+    Suppresslabel: true,
+    toolTipText: "date",
+    descDate: "date is imp",
+    Position: "left",
+    newLabel: "hello",
+  })
+);
 
+objarr.forEach(function (x, i) {
+  console.log(x);
+  $("body").append(x.getDesigen());
+});
 
-
-// objarr.push(new clsCreateInput({ "inputType": "date" }))
-
-// objarr.push(new clsCreateInput({ "inputType": "datetime" }))
-
-// objarr.push(new clsCreateInput({ "inputType": "time" }))
-// objarr.forEach(function (x,i){
-// console.log(x);
-// $("body").append(x.getDesigen())
-// })
-// console.log(objarr);
+objarr[0].initNow();
+console.log(objarr);
